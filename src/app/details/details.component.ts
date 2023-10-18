b054b2da5c8fe4e706481670ac4454d508ff6b81
Eric S <estriler@hotmail.com>
2023-10-18 17:17:56 -0500

    completed step 12, integrate angular forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -12,24 +12,38 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ReactiveFormsModule
     ],
-  template: `
-  <article>
-    <img class="listing-photo" [src]="housingLocation?.photo"
-      alt="Exterior photo of {{housingLocation?.name}}"/>
-    <section class="listing-description">
-      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
-      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
-    </section>
-    <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
-      <ul>
-        <li>Units available: {{housingLocation?.availableUnits}}</li>
-        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-      </ul>
-    </section>
-  </article>
-`,
+    template: `
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+  
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+  
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
+    </article>
+  `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
@@ -49,4 +63,13 @@ export class DetailsComponent {
       this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
   }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+  }
+
 }
